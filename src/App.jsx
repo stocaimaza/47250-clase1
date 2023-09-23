cf2b5c56e335967379615d34b8f1cf53894a8b65
stocaimaza <stocaimaza@hotmail.com>
2023-09-23 15:46:14 -0300

    clase 1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ProductoCard from './componentes/ProductoCard/ProductoCard';
+
+function App() {
+  
+  
+  return (
+    <>
+      <ProductoCard/>
+    </>
+  )
+}
+
+export default App
---
+++ src/componentes/ProductoInfo/ProductoInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ProductoInfo.css'
+
+const ProductoInfo = () => {
+  return (
+    <div className='product-info'>
+        <h1>Producto</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non eius enim sed vero minus. Inventore earum quod aut iusto, natus, explicabo consequatur maxime illo ducimus quo impedit totam, quisquam nesciunt.</p>
+        <p>$100</p>
+    </div>
+  )
+}
+
+export default ProductoInfo
